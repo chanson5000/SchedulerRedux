2940d0fc56cd6c01949732a08625ccd5d87d28fc
Cory <chanson5000@gmail.com>
2018-04-18 14:15:58 -0500

    Caliburn Micro MVVC basic layout

---
+++ ScheduleWhizRedux/ViewModels/ShellViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using ScheduleWhizRedux.Models;
+
+namespace ScheduleWhizRedux.ViewModels
+{
+    public class ShellViewModel: Conductor<object>
+    {
+        private string _firstName = "Cory";
+        private string _lastName;
+        private BindableCollection<PersonModel> _people = new BindableCollection<PersonModel>();
+        private PersonModel _selectedPerson;
+
+        public ShellViewModel()
+        {
+            People.Add(new PersonModel { FirstName = "Cory", LastName = "Hanson" });
+            People.Add(new PersonModel { FirstName = "Bill", LastName = "Jones" });
+            People.Add(new PersonModel { FirstName = "Sue", LastName = "Storm" });
+
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                _firstName = value; 
+                NotifyOfPropertyChange(() => FirstName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+                NotifyOfPropertyChange(() => LastName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+
+        public BindableCollection<PersonModel> People
+        {
+            get { return _people; }
+            set { _people = value; }
+        }
+
+        public PersonModel SelectedPerson
+        {
+            get { return _selectedPerson; }
+            set
+            {
+                _selectedPerson = value;
+                NotifyOfPropertyChange(() => SelectedPerson);
+            }
+        }
+
+        // Works automagically with clear text using Caliburn Micro.
+        // Its all about the naming conventions.
+        public bool CanClearText(string firstName, string lastName)
+        {
+            return !String.IsNullOrWhiteSpace(firstName) || !String.IsNullOrWhiteSpace(lastName);
+        }   
+
+        public void ClearText(string firstName, string lastName)
+        {
+            FirstName = "";
+            LastName = "";
+        }
+
+        public void LoadPageOne()
+        {
+            ActivateItem(new FirstChildViewModel());
+        }
+
+        public void LoadPageTwo()
+        {
+            ActivateItem(new SecondChildViewModel());
+        }
+
+
+    }
+
+}
--- ScheduleWhizRedux/Views/SecondChildView.xaml.cs
+++ ScheduleWhizRedux/Views/SecondChildView.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ScheduleWhizRedux
+namespace ScheduleWhizRedux.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SecondChildView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SecondChildView : UserControl
     {
-        public MainWindow()
+        public SecondChildView()
         {
             InitializeComponent();
         }
