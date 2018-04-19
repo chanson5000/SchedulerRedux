47d9922baf9a83970dc349e178c3f4771843355f
Cory <chanson5000@gmail.com>
2018-04-19 12:19:05 -0500

    Naming convention refactoring.

--- ScheduleWhizRedux/ViewModels/ShellViewModel.cs
+++ ScheduleWhizRedux/ViewModels/ShellViewModel.cs
@@ -13,34 +13,18 @@ namespace ScheduleWhizRedux.ViewModels
 {
     public class ShellViewModel: Conductor<object>
     {
-        private BindableCollection<Employee> _people = new BindableCollection<Employee>();
+        private BindableCollection<Employee> _employees = new BindableCollection<Employee>();
         private BindableCollection<Employee> _peopleFound = new BindableCollection<Employee>();
+        private BindableCollection<Job> _allJobs = new BindableCollection<Job>();
+        private Employee _selectedEmployee;
         //private List<string> _availableJobs;
         //private List<string> _assignedJobs;
-        private BindableCollection<Job> _allJobs = new BindableCollection<Job>();
-        private Employee _selectedPerson;
-        //readonly DataAccess db = new DataAccess();
         
         public ShellViewModel()
         {
-            People = new BindableCollection<Employee>(DataAccess.GetAllPeople());
+            Employees = new BindableCollection<Employee>(DataAccess.GetAllPeople());
             AllJobs = new BindableCollection<Job>(DataAccess.GetAllJobs());
         }
-
-        //public void EmployeeJobs()
-        //{
-        //    AssignedJobs = DataAccess.GetEmployeeAssignedJobs(SelectedPerson.Id);
-        //    List<string> unAssignedJobs = new List<string>();
-        //    foreach (var job in AssignedJobs)
-        //    {
-        //        if (!AssignedJobs.Contains(job))
-        //        {
-        //            unAssignedJobs.Add(job);
-        //        }
-        //    }
-
-        //    AvailableJobs = unAssignedJobs;
-        //}
 
         public BindableCollection<Job> AllJobs
         {
@@ -48,32 +32,39 @@ namespace ScheduleWhizRedux.ViewModels
             set { _allJobs = value; }
         }
 
-        //public List<string> AvailableJobs
-        //{
-        //    get { return _availableJobs; }
-        //    set
-        //    {
-        //        _assignedJobs = value;
-        //        NotifyOfPropertyChange(() => AvailableJobs);
-        //    }
-        //}
-
-        //public List<string> AssignedJobs
-        //{
-        //    get { return _assignedJobs; }
-        //    set
-        //    {
-        //        _assignedJobs = value;
-        //        NotifyOfPropertyChange(() => AssignedJobs);
-        //    }
-        //}
-
-        public BindableCollection<Employee> People
+        public BindableCollection<Employee> Employees
         {
-            get { return _people; }
-            set { _people = value; }
+            get { return _employees; }
+            set { _employees = value; }
         }
 
+        public Employee SelectedEmployee
+        {
+            get { return _selectedEmployee; }
+            set
+            {
+                _selectedEmployee = value;
+                NotifyOfPropertyChange(() => SelectedEmployee);
+            }
+        }
+
+        // Works automagically with clear text using Caliburn Micro.
+        // Its all about the naming conventions.
+        // This code may be useful to have as a reference for later.
+        //public bool CanClearText(string firstName, string lastName)
+        //{
+        //    return !String.IsNullOrWhiteSpace(firstName) || !String.IsNullOrWhiteSpace(lastName);
+        //}   
+
+        //public void LoadPageOne()
+        //{
+        //    ActivateItem(new FirstChildViewModel());
+        //}
+
+        //public void LoadPageTwo()
+        //{
+        //    ActivateItem(new SecondChildViewModel());
+        //}
         //public BindableCollection<Employee> PeopleFound
         //{
         //    get { return _peopleFound; }
@@ -83,33 +74,6 @@ namespace ScheduleWhizRedux.ViewModels
         //        NotifyOfPropertyChange(() => PeopleFound);
         //    }
         //}
-
-        public Employee SelectedPerson
-        {
-            get { return _selectedPerson; }
-            set
-            {
-                _selectedPerson = value;
-                NotifyOfPropertyChange(() => SelectedPerson);
-            }
-        }
-
-        // Works automagically with clear text using Caliburn Micro.
-        // Its all about the naming conventions.
-        public bool CanClearText(string firstName, string lastName)
-        {
-            return !String.IsNullOrWhiteSpace(firstName) || !String.IsNullOrWhiteSpace(lastName);
-        }   
-
-        public void LoadPageOne()
-        {
-            ActivateItem(new FirstChildViewModel());
-        }
-
-        public void LoadPageTwo()
-        {
-            ActivateItem(new SecondChildViewModel());
-        }
 
     }
 
