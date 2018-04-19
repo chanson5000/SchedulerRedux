bb3c42c06ea13f513227cbbd89ab0fb34d677f61
Cory <chanson5000@gmail.com>
2018-04-19 16:10:24 -0500

    Added logic to update list when employee is added. Added RemoveEmployee functionality.

--- ScheduleWhizRedux/ViewModels/ShellViewModel.cs
+++ ScheduleWhizRedux/ViewModels/ShellViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Media.Imaging;
 using Caliburn.Micro;
@@ -11,7 +12,7 @@ using ScheduleWhizRedux.Models;
 
 namespace ScheduleWhizRedux.ViewModels
 {
-    public class ShellViewModel: Conductor<object>
+    public class ShellViewModel : Conductor<object>
     {
         private BindableCollection<Employee> _employees = new BindableCollection<Employee>();
         private BindableCollection<Employee> _peopleFound = new BindableCollection<Employee>();
@@ -19,10 +20,11 @@ namespace ScheduleWhizRedux.ViewModels
         private Employee _selectedEmployee;
         private Job _selectedJob;
         private readonly AddEmployeeViewModel addEmployeeViewModel;
+
         private readonly IWindowManager windowManager;
         //private List<string> _availableJobs;
         //private List<string> _assignedJobs;
-        
+
 
         public ShellViewModel()
         {
@@ -68,38 +70,69 @@ namespace ScheduleWhizRedux.ViewModels
             }
         }
 
-        public void AddEmployee()
+        public void UpdateEmployeeList()
         {
-            windowManager.ShowWindow(addEmployeeViewModel);
+            Employees = new BindableCollection<Employee>(DataAccess.GetAllEmployees());
         }
 
-        // Works automagically with clear text using Caliburn Micro.
-        // Its all about the naming conventions.
-        // This code may be useful to have as a reference for later.
-        //public bool CanClearText(string firstName, string lastName)
-        //{
-        //    return !String.IsNullOrWhiteSpace(firstName) || !String.IsNullOrWhiteSpace(lastName);
-        //}   
+        public void AddEmployee()
+        {
+            var result = windowManager.ShowDialog(addEmployeeViewModel);
+            if (result == true)
+            {
+                Employees = new BindableCollection<Employee>(DataAccess.GetAllEmployees());
+            }
+        }
 
-        //public void LoadPageOne()
-        //{
-        //    ActivateItem(new FirstChildViewModel());
-        //}
+        public void RemoveEmployee()
+        {
+            if (MessageBox.Show("Do you really want to remove the employee from the database? This cannot be undone.",
+                    "Remove Employee?",
+                    MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.No)
+            {
+                // Do nothing if user says no.
+            }
+            else
+            {
+                if (DataAccess.RemoveEmployee(SelectedEmployee))
+                {
+                    Employees = new BindableCollection<Employee>(DataAccess.GetAllEmployees());
+                    MessageBox.Show("The the employee was removed from the database.", "Operation Successful",
+                        MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Unable to remove the employee from the database.", "Error",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
 
-        //public void LoadPageTwo()
-        //{
-        //    ActivateItem(new SecondChildViewModel());
-        //}
-        //public BindableCollection<Employee> PeopleFound
-        //{
-        //    get { return _peopleFound; }
-        //    set
-        //    {
-        //        _peopleFound = value;
-        //        NotifyOfPropertyChange(() => PeopleFound);
-        //    }
-        //}
+            // Works automagically with clear text using Caliburn Micro.
+            // Its all about the naming conventions.
+            // This code may be useful to have as a reference for later.
+            //public bool CanClearText(string firstName, string lastName)
+            //{
+            //    return !String.IsNullOrWhiteSpace(firstName) || !String.IsNullOrWhiteSpace(lastName);
+            //}   
 
+            //public void LoadPageOne()
+            //{
+            //    ActivateItem(new FirstChildViewModel());
+            //}
+
+            //public void LoadPageTwo()
+            //{
+            //    ActivateItem(new SecondChildViewModel());
+            //}
+            //public BindableCollection<Employee> PeopleFound
+            //{
+            //    get { return _peopleFound; }
+            //    set
+            //    {
+            //        _peopleFound = value;
+            //        NotifyOfPropertyChange(() => PeopleFound);
+            //    }
+            //}
+        }
     }
-
 }
