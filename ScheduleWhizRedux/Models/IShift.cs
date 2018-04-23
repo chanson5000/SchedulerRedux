e60b7ebf03d4e481cd3c79c7d70530a2861366ee
Cory <chanson5000@gmail.com>
2018-04-23 12:30:27 -0500

    Added Interfaces also because it will help when I need single column results from queries.

---
+++ ScheduleWhizRedux/Models/IShift.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleWhizRedux.Models
+{
+    public interface IShift
+    {
+        string ShiftName { get; set; }
+    }
+}
