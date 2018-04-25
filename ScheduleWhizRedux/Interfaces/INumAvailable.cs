4be394d203d632cde32a76ed1abff369050a98a3
Cory <chanson5000@gmail.com>
2018-04-25 09:22:24 -0500

    Added 'Interfaces' directory.

--- ScheduleWhizRedux/Interfaces/INumAvailable.cs
+++ ScheduleWhizRedux/Interfaces/INumAvailable.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ScheduleWhizRedux.Models
+namespace ScheduleWhizRedux.Interfaces
 {
     public interface INumAvailable
     {
