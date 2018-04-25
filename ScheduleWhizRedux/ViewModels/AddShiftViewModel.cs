8b8996e9bcf69ea7b66618bfc43c56f31e69fef7
Cory <chanson5000@gmail.com>
2018-04-24 23:13:43 -0500

    Functionality for adding shifts.

---
+++ ScheduleWhizRedux/ViewModels/AddShiftViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using ScheduleWhizRedux.Helpers;
+using ScheduleWhizRedux.Models;
+
+namespace ScheduleWhizRedux.ViewModels
+{
+    public class AddShiftViewModel : Screen
+    {
+        private DayOfWeek _day;
+        private int _numAvailable;
+        private string _job;
+        private string _newShift;
+
+        public string NewShift
+        {
+            get { return _newShift; }
+            set { _newShift = value; }
+        }
+
+        public string Job
+        {
+            get { return _job; }
+            set { _job = value; }
+        }
+
+        public DayOfWeek Day
+        {
+            get { return _day; }
+            set { _day = value; }
+        }
+
+        public int NumAvailable
+        {
+            get { return _numAvailable; }
+            set { _numAvailable = value; }
+        }
+    
+        public void AddShift()
+        {
+            if (NewShift == null)
+            {
+                MessageBox.Show("Please do not leave any fields blank.", "Input Error",
+                    MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return;
+            }
+
+            if (DataAccess.AddShiftForJobOnDay(Day, Job, NewShift, NumAvailable))
+            {
+                MessageBox.Show($"The shift, {NewShift}, was added to the database.", "Operation Successful",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+                TryClose(true);
+            }
+            else
+            {
+                MessageBox.Show($"Unable to add the job, {NewShift}, to the database.", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public void Cancel()
+        {
+            TryClose(false);
+        }
+    }
+}
