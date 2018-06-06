81b0491e06d08e36d0b14fc0d3dd6676f96b1d39
Cory <chanson5000@gmail.com>
2018-06-06 14:10:14 -0500

    Made schedule generation async with UI feedback.

--- ScheduleWhizRedux/Views/ShellView.xaml.cs
+++ ScheduleWhizRedux/Views/ShellView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using ScheduleWhizRedux.Models;
 using ScheduleWhizRedux.Repositories;
@@ -27,15 +28,22 @@ namespace ScheduleWhizRedux.Views
             SelectedEmployee_AssignedJobs.UnselectAll();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void Generate_Button_Click(object sender, RoutedEventArgs e)
         {
+            // New scheduler object takes a list of employees and available shifts to schedule.
             Scheduler scheduler = new Scheduler(new EmployeeRepository().GetAllSorted(),
                 new AssignedShiftRepository().GetAll().Where(x => x.NumAvailable > 0)
                     .ToList());
- 
-            Schedule schedule = scheduler.Generate();
+
+            // Some UI feedback while we wait for the awaited task to complete.
+            BtnGenerate.IsEnabled = false;
+            TxtGenerateSchedule.Text = "Generating Schedule...";
+
+            Schedule schedule = await Task.Run(() => scheduler.Generate());
 
             schedule.OpenFile();
+            TxtGenerateSchedule.Text = "Generate Schedule";
+            BtnGenerate.IsEnabled = true;
         }
     }
 }
