46a89ec3c12e5b97e5a414eade9c3397476db19a
Cory Hanson <chanson5000@gmail.com>
2018-06-23 02:45:54 -0500

    Inverted if statement to reduce nesting.

--- ScheduleWhizRedux/Repositories/Repository.cs
+++ ScheduleWhizRedux/Repositories/Repository.cs
@@ -16,48 +16,46 @@ namespace ScheduleWhizRedux.Repositories
 
         public static void CheckIfDatabaseExists()
         {
-            if (!System.IO.File.Exists("Data\\swredata.db"))
+            if (System.IO.File.Exists("Data\\swredata.db")) return;
+            SQLiteConnection.CreateFile("Data\\swredata.db");
+
+            string sql = "SELECT 1; " +
+                         "PRAGMA foreign_keys=OFF; " +
+                         "BEGIN TRANSACTION; " +
+                         "CREATE TABLE [Jobs] (" +
+                         "  [Id] INTEGER  NOT NULL" +
+                         ", [JobTitle] text  NOT NULL" +
+                         ", CONSTRAINT [sqlite_master_PK_Jobs] PRIMARY KEY ([Id])" +
+                         " ); " +
+                         "CREATE TABLE [Employees] ( " +
+                         "  [Id] INTEGER  NOT NULL" +
+                         ", [FirstName] text  NOT NULL" +
+                         ", [LastName] text  NOT NULL" +
+                         ", [EmailAddress] text  NOT NULL" +
+                         ", [PhoneNumber] text  NOT NULL" +
+                         ", CONSTRAINT [sqlite_master_PK_Employees] PRIMARY KEY ([Id])" +
+                         " );" +
+                         "CREATE TABLE [AssignedShifts] (" +
+                         "  [Id] INTEGER  NOT NULL" +
+                         ", [DayOfWeek] bigint  NOT NULL" +
+                         ", [JobId] bigint  NOT NULL" +
+                         ", [ShiftName] text  NOT NULL" +
+                         ", [NumAvailable] bigint  NOT NULL" +
+                         ", CONSTRAINT [sqlite_master_PK_AssignedShifts] PRIMARY KEY ([Id])" +
+                         " );" +
+                         "CREATE TABLE [AssignedJobs] ( " +
+                         "  [Id] INTEGER  NOT NULL" +
+                         ", [EmployeeId] bigint  NOT NULL" +
+                         ", [JobId] bigint  NOT NULL" +
+                         ", CONSTRAINT [sqlite_master_PK_AssignedJobs] PRIMARY KEY ([Id])" +
+                         " );" +
+                         "COMMIT;";
+
+            using (var sqlite = new SQLiteConnection(Repository.ConnectionString))
             {
-                SQLiteConnection.CreateFile("Data\\swredata.db");
-
-                string sql = "SELECT 1; " +
-                             "PRAGMA foreign_keys=OFF; " +
-                             "BEGIN TRANSACTION; " +
-                             "CREATE TABLE [Jobs] (" +
-                             "  [Id] INTEGER  NOT NULL" +
-                             ", [JobTitle] text  NOT NULL" +
-                             ", CONSTRAINT [sqlite_master_PK_Jobs] PRIMARY KEY ([Id])" +
-                             " ); " +
-                             "CREATE TABLE [Employees] ( " +
-                             "  [Id] INTEGER  NOT NULL" +
-                             ", [FirstName] text  NOT NULL" +
-                             ", [LastName] text  NOT NULL" +
-                             ", [EmailAddress] text  NOT NULL" +
-                             ", [PhoneNumber] text  NOT NULL" +
-                             ", CONSTRAINT [sqlite_master_PK_Employees] PRIMARY KEY ([Id])" +
-                             " );" +
-                             "CREATE TABLE [AssignedShifts] (" +
-                             "  [Id] INTEGER  NOT NULL" +
-                             ", [DayOfWeek] bigint  NOT NULL" +
-                             ", [JobId] bigint  NOT NULL" +
-                             ", [ShiftName] text  NOT NULL" +
-                             ", [NumAvailable] bigint  NOT NULL" +
-                             ", CONSTRAINT [sqlite_master_PK_AssignedShifts] PRIMARY KEY ([Id])" +
-                             " );" +
-                             "CREATE TABLE [AssignedJobs] ( " +
-                             "  [Id] INTEGER  NOT NULL" +
-                             ", [EmployeeId] bigint  NOT NULL" +
-                             ", [JobId] bigint  NOT NULL" +
-                             ", CONSTRAINT [sqlite_master_PK_AssignedJobs] PRIMARY KEY ([Id])" +
-                             " );" +
-                             "COMMIT;";
-
-                using (var sqlite = new SQLiteConnection(Repository.ConnectionString))
-                {
-                    sqlite.Open();
-                    SQLiteCommand command = new SQLiteCommand(sql, sqlite);
-                    command.ExecuteNonQuery();
-                }
+                sqlite.Open();
+                SQLiteCommand command = new SQLiteCommand(sql, sqlite);
+                command.ExecuteNonQuery();
             }
         }
     }
